29da653a5f1e1760717f5918708674ec58304ef2
Kristina970 <khrystyna.mashchakevych@gmail.com>
2019-06-05 17:58:39 +0300

    honework 9 flask

---
+++ flask_homework/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, flash, url_for
+from flask_restful import abort
+
+from home import home
+from vegetables import vegetables
+from fruits import fruits
+
+app = Flask(__name__)
+
+app.register_blueprint(home)
+app.register_blueprint(vegetables)
+app.register_blueprint(fruits)
+
+
+@app.route('/interesting')
+def page_not_implemented():
+    abort(404)
+    return render_template('')
+
+
+@app.errorhandler(404)
+def error_404_handler(error):
+    return "This page is still under development, sorry for inconveniences"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
